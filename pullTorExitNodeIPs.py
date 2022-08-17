31f0941742efa8c89943711482985eea05e71c8a
Mario J <mjere003@gmail.com>
2022-08-17 16:41:14 -0400

    optimized features

--- pullTorExitNodeIPs.py
+++ pullTorExitNodeIPs.py
@@ -5,41 +5,58 @@ from checkIP import ipCheck
 import wget
 
 today = date.today()
-print(today)
 
-#TODO: Add date to naming convention for for downloaded torbulkexitlist 
+vName = f"verifiedTorList-{today}.txt"  # Name for verified IP list
+rName = f"needReviewTorList-{today}.txt"  # Name for IPs that need Review    
+
+
 # Downloads the latest Tor Bulk Exit List
 def getTorList():
     siteUrl = "https://check.torproject.org/torbulkexitlist"
-    bulkList = wget.download(siteUrl)
+    bulkList = wget.download(siteUrl, f"torBulkExitList-{today}.txt")
 
 #TODO: Create Master blocklist of confirmed IPs, and list of IPs that need to be verified
 
-# Opens the "torbulkexitlist" file that was created from getTorList()
-f = open("torbulkexitlist", "r")
+def createLists():
 
-#TODO: Add functionality to check daily API usage and use that as count
-count = 1
+    #TODO: Add functionality to check if torBulkExitList is present for today's date, if not, create it first then proceed 
 
-# Verified IP list and naming convention
-vName = f"verifiedTorList-{today}.txt"
-verifiedIPs = open(vName, "w")
+    # Opens the "torBulkExitList.txt" file that was created from getTorList()
+    f = open(f"torBulkExitList-{today}.txt", "r")
 
-# Checks if IPs from torbulkexitlist are identified as such in ABUSEIPDB
-for ip in f.readlines():
-    if count <= 10:
-        checkedIP = ipCheck(ip.strip())
-        if checkedIP != None:
-            print(f"IP: {ip.strip()} -- Hostname: {checkedIP}")
-            # Adds verified IP to verifiedIP file
-            verifiedIPs.write(ip)
-            print(f"Added {ip.strip()} to {vName}\n")
-        else:
-            print(f"IP: {ip.strip()} requires further checking, does not have 'TOR' in hostname.")
-        count+=1
+    #TODO: Add functionality to check daily API usage and use that as count
+    count = 1
+
+    # Verified IP list and naming convention
+    verifiedIPs = open(vName, "w")
+    ipsNeedReview = open(rName, "w")
+
+    reviewIPsSet = []
+
+    # Checks if IPs from torbulkexitlist are identified as such in ABUSEIPDB
+    for ip in f.readlines():
+        if count <= 10:
+            checkedIP = ipCheck(ip.strip())
+            if checkedIP != None:
+                #print(f"IP: {ip.strip()} -- Hostname: {checkedIP}")
+                # Adds verified IP to verifiedIP file
+                verifiedIPs.write(ip)
+                
+            else:
+                #print(f"IP: {ip.strip()} requires further checking, does not have 'TOR' in hostname.")
+                reviewIPsSet.append(ip.strip())
+                ipsNeedReview.write(ip)
+            count+=1
 
 
-f.close()
-verifiedIPs.close()
+    f.close()
+    verifiedIPs.close()
+    ipsNeedReview.close()
+
+    return reviewIPsSet
 
 #TODO: Add functionality to check if IPs are still associated with Tor, if not, remove them
+
+print(f"{vName} was created.")
+print("The following IPs need review:")
+print(createLists())
