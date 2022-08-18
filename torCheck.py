32f31db899b64c1243e584b8af0fec67d0851fe8
Mario J <mjere003@gmail.com>
2022-08-18 15:33:31 -0400

    added log writing capabilities

--- torCheck.py
+++ torCheck.py
@@ -5,12 +5,11 @@ from os import path
 from datetime import date, timedelta
 
 
-
 def getTorList():
 
     today = date.today()
     yesterday = today - timedelta(days=1)
-
+    
     try:
         if path.isfile(f"torBulkExitList-{today}.txt") is False:
             siteUrl = "https://check.torproject.org/torbulkexitlist"
@@ -24,18 +23,38 @@ def getTorList():
         todayTor = set()
         yesterdayTor = set()
 
-        for x in open(f"torBulkExitList-{today}.txt", "r").readlines():
+        todaysList = open(f"torBulkExitList-{today}.txt", "r")
+        for x in todaysList.readlines():
             todayTor.add(x.strip())
 
-        for y in open(f"torBulkExitList-{yesterday}.txt", "r").readlines():
+        yesterdaysList = open(f"torBulkExitList-{yesterday}.txt", "r")
+        for y in yesterdaysList.readlines():
             yesterdayTor.add(y.strip())
 
-        new = [ip for ip in todayTor if ip not in yesterdayTor] # Checks if an ip is in today's list that is not in yesterday's
-        
-        return new
-    
+        #TODO: Add write to log file here +--------------------------+
+        #new = [ip for ip in todayTor if ip not in yesterdayTor] # Checks if an ip is in today's list that is not in yesterday's
+        logEntries = {
+        "Date" : today,
+        "IP Change" : [ip for ip in todayTor if ip not in yesterdayTor],
+        }
+
+        torLog = open("torLog.txt", "a")
+
+        for k,v in logEntries.items():
+            torLog.write(f"{k} : \n{v}")
+        torLog.write("[+]---------------------------[+]\n")
+
+
+
+        todaysList.close()
+        yesterdaysList.close()
+        torLog.close()
+
+        return torLog
+  
     except Exception as e:
         print(e)
+
 
 print(getTorList())
 
